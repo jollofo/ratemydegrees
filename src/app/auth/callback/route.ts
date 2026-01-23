48316ac26b0a50fd2572dff4c50ba758fec9c97a
jollofo <jmuguiyijr@gmail.com>
2026-01-22 23:09:02 -0500

    Fix: missing db entries

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -15,5 +15,5 @@ export async function GET(request: Request) {
     }
 
     // URL to redirect to after sign up process completes
-    return NextResponse.redirect(`https://ratemydegrees-production.up.railway.app/`)
+    return NextResponse.redirect(`${origin}/`)
 }
